fe7edcb7af12f568f6ae8b8c6977975c874184c3
Vehruni Judistry <rema2judistry@gmail.com>
2018-11-06 23:10:44 +0000

    rps

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 // *************************** YOUR CODE BELOW *******************************
+
+$("#shoot").click(function() {
+    $("#userChoice").text($("#input").val());
+});
+
+
+
+
+
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
