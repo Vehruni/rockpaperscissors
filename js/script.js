078cff7a6605da8c27371c12e5c918254c47200b
Vehruni Judistry <rema2judistry@gmail.com>
2018-11-20 23:25:35 +0000

    last day

--- js/script.js
+++ js/script.js
@@ -3,22 +3,75 @@
 $("#shoot").click(function() {
   userChoice = $("input").val()
   var randomNumber = Math.random()
+  var computerChoice = ""
+  var wins;
+  
     $("#userChoice").text(userChoice)
     
     if(randomNumber> .80) {
+        computerChoice="Rock"
         $("#computerChoice").text("Rock"); 
     }
     
        else if(randomNumber <.80 && randomNumber>.50) {
+           computerChoice="Paper"
         $("#computerChoice").text("Paper"); 
     }
     
        else{
-        $("#computerChoice").text("Scissor"); 
+        $("#computerChoice").text("Scissor");
+        computerChoice="Scissor"
     }
     
+    if (userChoice==="scissor" && computerChoice==="Rock") {  
     
+        wins=" Computer Wins"
+    }
     
+    if (userChoice==="paper" && computerChoice==="Scissor"){
+        
+         wins="Computer Wins"
+    }
+    
+    if (userChoice==="paper" && computerChoice==="Rock"){
+        
+         wins="User Wins"
+        
+    }
+    
+    if (userChoice==="rock" && computerChoice==="Scissor"){
+        
+         wins="User Wins"
+    }
+    
+    if (userChoice==="rock" && computerChoice==="Paper"){
+        
+         wins="Computer Wins"
+    }
+    
+    if (userChoice==="scissor" && computerChoice==="Paper"){
+        
+         wins=" User Wins"
+    }
+    
+    if (userChoice==="rock" && computerChoice==="Rock"){
+        
+         wins="Try Again"
+    }
+    
+    if (userChoice==="scissor" && computerChoice==="Scissor"){
+        
+         wins="Try Again"
+    }
+    
+    if (userChoice==="paper" && computerChoice==="Paper"){
+        
+         wins="Try Again"
+    }
+    
+    $("#result").text(wins);
+    
+    $("#input").text()
     
     
     
